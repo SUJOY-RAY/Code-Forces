262be3ebbe0ba1bafad8d062870e2e36cc507d36
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-07-06 10:31:57 +0530

    158A
    "Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..." — an excerpt from contest rules.
    
    A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
    
    Input
    The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
    
    The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
    
    Output
    Output the number of participants who advance to the next round.
    
    Examples
    input
    8 5
    10 9 8 7 7 7 5 5
    output
    6

---
+++ Next Round.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n,k;
+	cout<<"Enter the no of contestants and then the positon "<<endl;
+	cin>>n>>k;
+	int arr[n];
+	int count=0;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<n;i++){
+		if(arr[i]>=arr[k] && arr[i]>0){
+			count++;
+		}	
+	}
+	cout<<count;
+	
+	return 0;
+}
