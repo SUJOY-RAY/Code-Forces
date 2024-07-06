fd8048a30b1d2531c56ddd6e86efdc3ad37f5090
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-07-06 10:42:36 +0530

    Next Round.cpp

--- Next Round.cpp
+++ Next Round.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main(){
 	int n,k;
-	cout<<"Enter the no of contestants and then the positon "<<endl;
+	//cout<<"Enter the no of contestants and then the positon "<<endl;
 	cin>>n>>k;
 	int arr[n];
 	int count=0;
-	for(int i=0;i<n;i++){
+	for(int i=1;i<=n;i++){
 		cin>>arr[i];
 	}
-	for(int i=0;i<n;i++){
+	for(int i=1;i<=n;i++){
 		if(arr[i]>=arr[k] && arr[i]>0){
 			count++;
 		}	
