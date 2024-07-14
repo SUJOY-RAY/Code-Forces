492786a5ea9ed5ed5c0b3a3010f625fb39f1f920
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-07-14 20:17:47 +0530

    486A

---
+++ Calculating Function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	long long n,result;
+	cin>>n;
+	
+	if (n%2==0){
+		result=n/2;
+	}
+	else{
+		result=((n+1)/2)*(-1);
+	}
+	cout<<result;
+	
+	return 0;
+}
