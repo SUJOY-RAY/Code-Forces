99b75f6948931bf90c65a87918b534fbe78ebeae
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-12-25 09:14:48 +0530

    Add A. Destroying Bridges solution in Java

---
+++ A. Destroying Bridges.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- >0){
+            int n = sc.nextInt();
+            long k = sc.nextLong();
+            
+            if(k >= n - 1) {
+                System.out.println(1);
+            }else{
+                System.out.println(n);
+            }
+        }
+    }
+}
